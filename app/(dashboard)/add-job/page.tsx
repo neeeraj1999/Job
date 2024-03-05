06f8a20e16ccdae43e37eb1e63f7df03d14c43e9
Kaustubh Desai <119646763+kndesai@users.noreply.github.com>
2024-03-05 14:31:45 -0500

    Edit 2

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,6 +1,16 @@
+import CreateJobForm from '@/components/CreateJobForm';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+
 function AddJobPage() {
-    return <h1 className="text-4xl">
-        AddJobPage
-    </h1>
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CreateJobForm />
+    </HydrationBoundary>
+  );
 }
 export default AddJobPage;
